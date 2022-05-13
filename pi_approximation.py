f323444fdd2f3dfa6ebfe6422428b7819e5c9257
Bailey Muckel <bailey.muckel@greenwayhealth.com>
2022-05-13 16:13:56 -0400

    Added arg parsing

--- pi_approximation.py
+++ pi_approximation.py
@@ -4,18 +4,22 @@ import sys
 def main(argv):
     num_points = get_input_as_int("How many points should be generated? ")
     
+    if '-v' in argv:
+        is_verbose = True
+    else:
+        is_verbose = False
+    
     
 
 
 def get_input_as_int(message: str, error_message: str = "Not an integer, try again") -> int:
     while True:
         try:
-            ret_value = int(input(message))
+            return int(input(message))
         except ValueError:
             print(error_message)
             continue
-        break
 
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    main(sys.argv)
